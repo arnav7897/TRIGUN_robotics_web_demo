bbf1471e27f218164085d0cbf2fc5672b2d1f258
Arnav Sharda <asharda7898@gmail.com>
2025-03-29 13:14:20 +0530

    added the navbar and also made a hero session

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,34 @@
 import React from "react";
-
+import './hero.css'
+import Loader from "./heroanimation";
+import Loader2 from "./heroanimation2";
 const Hero = () => {
-  return <div>Hero</div>;
+  return<>
+  <section className="w-full h-screen bg-green-200 spacer layer5 flex">
+      <div className=' mt-30'>
+      <Loader />
+      </div>
+
+      <div className="w-full items-center h-3/4 flex flex-col justify-center ">
+        <div className="upper-text text-8xl">
+        Collaborate Effectively,
+        </div>
+        <div className="upper-text text-8xl">
+        and Beautifully.
+        </div>
+        <div className="upper-text text-3xl mt-10">
+        DueTo provides an effective and joyful way to manage your creative projects
+        </div>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+      </div>
+
+      <div className='mr-4 mt-30'>
+      </div>
+  </section>
+  </>
 };
 
 export default Hero;
