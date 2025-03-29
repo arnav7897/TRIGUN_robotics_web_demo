ea102f8c7466bd76c8499a9490c252ab3c25a553
Rushikesh Iche <138008769+RushikeshIche@users.noreply.github.com>
2025-03-29 10:27:48 +0530

    Component files created and Contact Form frontend template initiated

---
+++ src/components/Contact.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ContactForm from "./ContactForm";
+
+const Contact = () => {
+  return (
+    <div className="bg-violet-300 pt-8">
+      <div className="mx-20">
+        <h3 className="text-2xl mb-8">Ready to get started?</h3>
+        <h1 className="text-5xl mb-12">
+          Discover a new era of banking.
+          <br />
+          Reach out to start your journey today.
+        </h1>
+
+        <ContactForm />
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
